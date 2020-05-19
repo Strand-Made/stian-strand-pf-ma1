0bf5e097353ec47bc11588f0b18b3c27a019668d
Stian Strand <stianbekkelund@hotmail.com>
2020-05-19 20:58:46 +0200

    pf ma01 This is the module assigment

---
+++ script.js
@@ -0,0 +1,58 @@
+// question 1
+
+//your answer for question 1 goes here
+var firstVar;
+
+//question 2
+
+//your answer for question 2 goes here
+var name = "Stian";
+
+//question 3
+
+//your answer for question 3 goes here
+var numberValue = 25;
+
+//question 4
+
+//your answer for question 4 goes here
+var division = 20 / 5;
+
+//question 5
+
+//your answer for quesiton 5 goes here
+
+var isItFrog = "frog";
+typeof isItFrog;
+
+//question 6
+
+//your answer for quesiton 6 goes here
+
+var orderHasShipped = true;
+
+//question 7
+
+//your answer for quesiton 7 goes here
+
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+//question 8
+
+//your answer for quesiton 8 goes here
+
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+//question 9
+
+//your answer for quesiton 9 goes here
+
+//question 10
+
+//your answer for quesiton 10 goes here
